e2dcdd38bf1f4051809ed3bb2caeaafbb3240501
AntiqueN <anton.navumuk@gmail.com>
2024-10-27 16:50:25 +0100

    TopDown template

---
+++ Source/DarknessDemo/DarknessDemo.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DarknessDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DarknessDemo, "DarknessDemo" );
+
+DEFINE_LOG_CATEGORY(LogDarknessDemo)
+ 
