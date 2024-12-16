52a6a38a93109e3595b16f641fce88d1a6b584ed
AntiqueN <anton.navumuk@gmail.com>
2024-12-16 23:34:44 +0100

    added simple editor building scripts

---
+++ build_shipping_editor.bat
@@ -0,0 +1,2 @@
+call winenv.cmd
+"%DD_ENGINE_PATH%\Engine\Build\BatchFiles\Build.bat" DarknessDemoEditor Win64 Development "%DD_PROJECT_PATH%"
---
+++ build_shipping_editor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source ./macbash.env
+
+# Access the variables
+"$DD_ENGINE_PATH/Engine/Build/BatchFiles/Mac/Build.sh" Editor Mac Development "$DD_PROJECT_PATH"
